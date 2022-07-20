9767aa8bbb00d58bd661ab918a8dcbf83015d74a
sudoparsa <sp.hoseiny@gmail.com>
2022-07-21 00:25:51 +0430

    add transformers backend add fasttext backend

---
+++ informationretrieval/retrieval_systems/retrieval_fasttext.py
@@ -0,0 +1,76 @@
+import json
+import time
+from .preprocess import Preprocess
+import numpy as np
+import os
+from gensim.models import KeyedVectors
+from scipy.special import softmax
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class FastTextRetrieval:
+    def __init__(self,
+                 word_vectors_path='models/FastText/word_vectors.kv',
+                 ngram_path='models/FastText/word_vectors.kv.vectors_ngrams.npy',
+                 embedding_path='models/FastText/doc_embedding.json',
+                 term2idf_path='models/FastText/term2idf.json'):
+        print('Loading FastText...')
+        if not os.path.isfile(ngram_path):
+            self.download_model(ngram_path)
+        self.word_vectors = KeyedVectors.load(word_vectors_path)
+        self.doc_embedding = json.load(open(embedding_path))
+        self.term2idf = json.load(open(term2idf_path))
+        self.preprocessor = Preprocess()
+        self.embedding_size = len(self.doc_embedding['embedding'][0])
+
+    def download_model(self, path):
+        id = '1--kLTUB5UBF_ED1UhGNrkagduXfUoNtA'
+        os.chdir(os.path.dirname(path))
+        os.system('gdown 1--kLTUB5UBF_ED1UhGNrkagduXfUoNtA')
+        os.chdir('../' * len(os.path.dirname(path).split('/')))
+
+    def show(self, indexes, scores):
+        result = []
+        print('\n')
+        print('Similar Papers using Cosine Similarity:')
+        for ix, i in zip(indexes, range(len(indexes))):
+            print(f'\n{i}.', end='')
+            print(f' {self.doc_embedding["title"][ix]}')
+            print(f'Cosine Similarity : {scores[ix]}')
+            result.append({'title': self.doc_embedding["title"][ix],
+                           'url': self.doc_embedding["url"][ix],
+                           'score': scores[ix]})
+        print()
+        return result
+
+    def embed(self, text):
+        vectors = []
+        weights = []
+        for term in text.split():
+            if term in self.term2idf.keys():
+                vectors.append(np.array(self.word_vectors[term]))
+                weights.append(self.term2idf[term])
+        if len(weights) == 0:
+            return np.zeros(self.embedding_size)
+        weights = np.array(softmax(weights))
+        vectors = np.array(vectors)
+        return weights @ vectors
+
+    def most_similar(self, query, k):
+        clean_query = self.preprocessor.run(query)
+        query_embedding = self.embed(clean_query).reshape(1, -1)
+        embeddings = self.doc_embedding['embedding']
+        cosine_scores = cosine_similarity(query_embedding, embeddings)[0]
+        similar_ix = np.argsort(cosine_scores)[::-1][:k]
+        return similar_ix, cosine_scores
+
+    def run(self, query, section='abstract', k=10):
+        start_time = time.time()
+        print(f'Query: {query}')
+        indx, scores = self.most_similar(query, k)
+        result = self.show(indx, scores=scores)
+        print(f'Execution time: {time.time() - start_time}')
+        return result
+
+
+fasttext_model = FastTextRetrieval()
---
+++ informationretrieval/retrieval_systems/retrieval_transformers.py
@@ -0,0 +1,48 @@
+import json
+import time
+from .preprocess import Preprocess
+import numpy as np
+from sentence_transformers import SentenceTransformer, util
+
+
+class TransformerRetrieval:
+    def __init__(self,
+                 model_path='models/SentenceTransformers/model/',
+                 embedding_path='models/SentenceTransformers/doc_embedding.json'):
+        print('Loading SentenceTransformer...')
+        self.model = SentenceTransformer(model_path)
+        self.doc_embedding = json.load(open(embedding_path))
+        self.preprocessor = Preprocess()
+
+    def show(self, indexes, scores):
+        result = []
+        print('\n')
+        print('Similar Papers using Cosine Similarity:')
+        for ix, i in zip(indexes, range(len(indexes))):
+            print(f'\n{i}.', end='')
+            print(f' {self.doc_embedding["title"][ix]}')
+            print(f'Cosine Similarity : {scores[ix]}')
+            result.append({'title': self.doc_embedding["title"][ix],
+                           'url': self.doc_embedding["url"][ix],
+                           'score': scores[ix]})
+        print()
+        return result
+
+    def most_similar(self, query, k):
+        query = self.preprocessor.run(query)
+        query_embedding = self.model.encode(query)
+        embeddings = self.doc_embedding['embedding']
+        cosine_scores = util.dot_score(query_embedding, embeddings).detach().cpu().numpy()[0]
+        similar_ix = np.argsort(cosine_scores)[::-1][:k]
+        return similar_ix, cosine_scores
+
+    def run(self, query, section='abstract', k=10):
+        start_time = time.time()
+        print(f'Query: {query}')
+        indx, scores = self.most_similar(query, k)
+        result = self.show(indx, scores=scores)
+        print(f'Execution time: {time.time() - start_time}')
+        return result
+
+
+transformer_model = TransformerRetrieval()
